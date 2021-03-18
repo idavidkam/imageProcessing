4be878874b9d47b3f78d99127a730f6718784f51
mnyesayev <m.n.y.121416@gmail.com>
2021-03-18 17:07:02 +0200

    finish sphere

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -2,6 +2,7 @@ package geometries;
 
 import primitives.Point3D;
 import primitives.Vector;
+
 /**
  * A class representing a Sphere in a three-dimensional Cartesian system
  * 
@@ -11,16 +12,19 @@ public class Sphere implements Geometry {
 
 	private Point3D center;
 	private double radius;
-	
+
 	/**
 	 * getter center of sphere
+	 * 
 	 * @return a center of sphere
 	 */
 	public Point3D getCenter() {
 		return center;
 	}
+
 	/**
 	 * getter radius of sphere
+	 * 
 	 * @return a radius of sphere
 	 */
 	public double getRadius() {
@@ -28,7 +32,8 @@ public class Sphere implements Geometry {
 	}
 
 	/**
-	 * Sphere constructor receiving a Point3d value to center and radius of Sphere 
+	 * Sphere constructor receiving a Point3d value to center and radius of Sphere
+	 * 
 	 * @param center - a center of sphere
 	 * @param radius - a radius of sphere
 	 */
@@ -39,8 +44,7 @@ public class Sphere implements Geometry {
 
 	@Override
 	public Vector getNormal(Point3D point) {
-		// TODO Auto-generated method stub
-		return null;
+		return point.subtract(center).normalize();
 	}
 
 	@Override
