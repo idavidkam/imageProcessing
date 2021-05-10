7f4723778e3a1448e1c3b8dc1fa6beb8eaf80faf
mnyesayev <m.n.y.121416@gmail.com>
2021-05-10 19:54:58 +0300

    up

--- src/unittests/ShadowTests.java
+++ src/unittests/ShadowTests.java
@@ -80,7 +80,7 @@ public class ShadowTests {
 						.setEmission(new Color(java.awt.Color.BLUE)) //
 						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)) //
 		);
-		scene.lights.add( //
+		scene.lights.add(
 				new SpotLight(new Color(400, 240, 0), new Point3D(-100, -100, 200), new Vector(1, 1, -3)) //
 						.setKl(1E-5).setKq(1.5E-7));
 
